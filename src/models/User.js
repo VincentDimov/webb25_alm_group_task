ae06b06157c6daaf11ca4ed17fd44fc65ca05495
mrshed21 <zhm.othman@gmail.com>
2026-05-21 18:20:17 +0200

    Refactor codebase to use ES6 module syntax across tests and models

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
@@ -34,4 +34,4 @@ userSchema.pre("findOneAndDelete", async function (next) {
   next();
 });
 
-module.exports = mongoose.model("User", userSchema);
+export default mongoose.model("User", userSchema);
